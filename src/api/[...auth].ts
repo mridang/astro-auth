f4199390f7fa3bd2c56b8b5a1d2fd05e7ce08e94
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2025-11-05 11:44:12 +0700

    chore(test): exclude api route from code coverage
    The src/api/[...auth].ts file is a simple API entrypoint that
    re-exports the main handler. It doesn't contain any testable logic
    itself, so it's being excluded from the coverage report.
    
    This is done using an `/* istanbul ignore file */` comment.

--- src/api/[...auth].ts
+++ src/api/[...auth].ts
@@ -1,5 +1,7 @@
+/* istanbul ignore file */
 import { AstroAuth } from '../server';
 
+// noinspection JSUnusedGlobalSymbols
 export const prerender = false;
 
 export const { GET, POST } = AstroAuth();
